716261158dabe896d41b27f31e77fbaa7324a62f
moyshik7 <moyshik7@gmail.com>
2022-06-09 22:08:24 +0600

    Basic files v2

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
-console.log("Hello")
+import dotenv from "dotenv"
+dotenv.config()
+
+import { Client, CommandInteractionOptionResolver } from "discord.js"
+
+const client = new Client({
+    intents: [
+        "GUILDS",
+        "GUILD_MESSAGES"
+    ]
+})
+
+client.on("ready", () => {
+    console.log("Bot ready")
+})
+
+client.on("interactionCreate", interaction => {
+    if(interaction.isCommand()){
+        /**
+         * Command use
+         */
+        if(interaction.commandName === "help"){
+            return interaction.reply("no")
+        }
+    }
+    if(interaction.isButton()){
+        /**
+         * Button press
+         */
+    }
+})
+
+client.login(process.env.BOT_TOKEN)
