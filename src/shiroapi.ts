7f7b1fbb53d79e68d4cab9b948e50a6daf78c056
moyshik7 <moyshik7@gmail.com>
2022-06-13 20:42:31 +0600

    Added shiro features

---
+++ src/shiroapi.ts
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const BASE_URL = "https://api.dbot.dev"
+
+export const GetShiroRaw = (endpoint: string): Promise<string> => {
+    return new Promise((
+        resolve: (any) => void, 
+        reject: (any) => void
+    ) => {
+        axios({
+            url: `${BASE_URL}/${endpoint}`,
+            method: "GET"
+        }).then(res => {
+            resolve(res.data.url)
+        }).catch(reject)
+    })
+}
