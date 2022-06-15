be0924b8c1e35f2baad2a747f96e361b5a0cfda8
moyshik7 <moyshik7@gmail.com>
2022-06-15 19:25:06 +0600

    Probably added nom command

---
+++ src/actions/commands/nom.ts
@@ -0,0 +1,24 @@
+import {
+    CommandInteraction,
+    MessageEmbed
+} from "discord.js";
+import { GetShiroRaw } from "../../shiroapi";
+
+
+export const NomCommand = async (interaction: CommandInteraction): Promise<void> => {
+    try {
+        await interaction.deferReply()
+
+        const image = await GetShiroRaw("/images/nom")
+
+        const embed = new MessageEmbed()
+            .setTitle(`nom nom`)
+            .setImage(image)
+            .setColor("#ff6f61")
+
+        interaction.editReply({
+            embeds: [ embed ]
+        })
+        return;
+    } catch(err){ console.log(err) }
+}
