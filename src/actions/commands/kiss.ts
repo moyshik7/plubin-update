a407337c227e8ab7ad9774a76e82d2d84fb56eed
moyshik7 <moyshik7@gmail.com>
2022-06-23 15:18:42 +0600

    Added kiss command

---
+++ src/actions/commands/kiss.ts
@@ -0,0 +1,35 @@
+import {
+    CommandInteraction,
+    MessageEmbed
+} from "discord.js";
+import { GetShiroRaw } from "../../shiroapi";
+
+
+export const KissCommand = async (interaction: CommandInteraction): Promise<void> => {
+    try {
+        await interaction.deferReply()
+
+        const user = interaction.user;
+        const victim = interaction?.options.get("user", false)?.user;
+
+        let message: string;
+
+        if(!victim){
+            message = `${user.username} tries to kiss themself\n(ayo wtf)`
+        } else {
+            message = `${user.username} kisses ${victim.username}\nCan you not do in public`
+        }
+
+        const image = await GetShiroRaw("/images/kiss")
+
+        const embed = new MessageEmbed()
+            .setDescription(message)
+            .setImage(image)
+            .setColor("#ff6f61")
+
+        interaction.editReply({
+            embeds: [ embed ]
+        })
+        return;
+    } catch(err){ console.log(err) }
+}
