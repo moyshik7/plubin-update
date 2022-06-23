4a9eec6eb4645b76f16ba79ee7267f309485211f
moyshik7 <moyshik7@gmail.com>
2022-06-23 15:13:14 +0600

    Added hug command

---
+++ src/actions/commands/hug.ts
@@ -0,0 +1,35 @@
+import {
+    CommandInteraction,
+    MessageEmbed
+} from "discord.js";
+import { GetShiroRaw } from "../../shiroapi";
+
+
+export const HugCommand = async (interaction: CommandInteraction): Promise<void> => {
+    try {
+        await interaction.deferReply()
+
+        const user = interaction.user;
+        const victim = interaction?.options.get("user", false)?.user
+
+        let message: string;
+
+        if(!victim){
+            message = `${user.username} tries to hug themself\n(Get a life you lonely freak)`
+        } else {
+            message = `${user.username} hugs ${victim.username}\nHow cute`
+        }
+
+        const image = await GetShiroRaw("/images/hug")
+
+        const embed = new MessageEmbed()
+            .setDescription(message)
+            .setImage(image)
+            .setColor("#ff6f61")
+
+        interaction.editReply({
+            embeds: [ embed ]
+        })
+        return;
+    } catch(err){ console.log(err) }
+}
