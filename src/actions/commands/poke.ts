a8076553312fe4c8ad668212931e13a790a96b6c
moyshik7 <moyshik7@gmail.com>
2022-06-24 20:38:34 +0600

    Added poke command

---
+++ src/actions/commands/poke.ts
@@ -0,0 +1,35 @@
+import {
+    CommandInteraction,
+    MessageEmbed
+} from "discord.js";
+import { GetShiroRaw } from "../../shiroapi";
+
+
+export const PokeCommand = async (interaction: CommandInteraction): Promise<void> => {
+    try {
+        await interaction.deferReply()
+
+        const user = interaction.user;
+        const victim = interaction?.options.get("user", false)?.user;
+
+        let message: string;
+
+        if(!victim || (victim.id === user.id)){
+            message = `${user.username} just because you can doesn't mean you have to do it`
+        } else {
+            message = `${user.username} keeps poking ${victim.username}\n${victim.username} is getting annoyed`
+        }
+
+        const image = await GetShiroRaw("/images/poke")
+
+        const embed = new MessageEmbed()
+            .setDescription(message)
+            .setImage(image)
+            .setColor("#ff6f61")
+
+        interaction.editReply({
+            embeds: [ embed ]
+        })
+        return;
+    } catch(err){ console.log(err) }
+}
