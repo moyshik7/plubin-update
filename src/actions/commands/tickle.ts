2a396e7d55a17e07c3cd2f12da9f3f2cbd3a5f3d
moyshik7 <moyshik7@gmail.com>
2022-06-24 20:46:01 +0600

    Added tickle command

---
+++ src/actions/commands/tickle.ts
@@ -0,0 +1,35 @@
+import {
+    CommandInteraction,
+    MessageEmbed
+} from "discord.js";
+import { GetShiroRaw } from "../../shiroapi";
+
+
+export const TickleCommand = async (interaction: CommandInteraction): Promise<void> => {
+    try {
+        await interaction.deferReply()
+
+        const user = interaction.user;
+        const victim = interaction?.options.get("user", false)?.user;
+
+        let message: string;
+
+        if(!victim || (victim.id === user.id)){
+            message = `${user.username} tickles themself/nNoone laughed`
+        } else {
+            message = `${user.username} tickles ${victim.username}\n${victim.username} dies from laughter`
+        }
+
+        const image = await GetShiroRaw("/images/tickle")
+
+        const embed = new MessageEmbed()
+            .setDescription(message)
+            .setImage(image)
+            .setColor("#ff6f61")
+
+        interaction.editReply({
+            embeds: [ embed ]
+        })
+        return;
+    } catch(err){ console.log(err) }
+}
