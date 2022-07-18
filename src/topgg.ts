275210bcc80c9aaa3367075de35d0288d1f48c67
moyshik7 <moyshik7@gmail.com>
2022-07-18 18:13:57 +0600

    Made things a bit better looking

--- src/topgg.ts
+++ src/topgg.ts
@@ -1,10 +1,13 @@
-import { Api } from "@top-gg/sdk"
+import { Api, BotStats } from "@top-gg/sdk"
 import { Client } from "discord.js"
 
 const api = new Api(process.env.TOPGG_API)
 
 export const TopggStatusHandler = (client: Client): void => {
     try {
+        if(process.env.PRODUCTION === "N"){
+            PostTopggStats(client).catch(console.log)
+        }
         setInterval(() => {
         try {
             if(process.env.PRODUCTION !== "N"){
@@ -14,14 +17,22 @@ export const TopggStatusHandler = (client: Client): void => {
                 console.log(client.guilds.cache.size)
                 return;
             }
-            api.postStats({
-                serverCount: client.guilds.cache.size
-            }).then((stats) => {
-                console.log(`Posted status on top.gg Servers: ${stats.serverCount}`)
-            }).catch(console.log)
+            PostTopggStats(client)
+                .then((stats) => {
+                    console.log(`Posted status on top.gg Servers: ${stats.serverCount}`)
+                })
+                .catch(console.log)
         }catch(err){console.log(err)}
         }, 30 * 60 * 1000)
     } catch (err){
         console.log(err)
     }
+}
+
+export const PostTopggStats = (client: Client): Promise<BotStats> => {
+    return new Promise((resolve, reject) => {
+        api.postStats({
+            serverCount: client.guilds.cache.size
+        }).then(resolve).catch(reject)
+    })
 }
