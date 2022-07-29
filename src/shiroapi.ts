8cecdb6a72f434b29fb42003d0ff16ba28c4fc6e
Akio <58624651+moyshik7@users.noreply.github.com>
2022-07-29 15:25:32 +0600

    Added sentry 7.8.0

--- src/shiroapi.ts
+++ src/shiroapi.ts
@@ -4,7 +4,7 @@ const BASE_URL = "https://api.dbot.dev"
 
 export const GetShiroRaw = (endpoint: string): Promise<string> => {
     return new Promise((
-        resolve: (any) => void, 
+        resolve: (any) => void,
         reject: (any) => void
     ) => {
         axios({
